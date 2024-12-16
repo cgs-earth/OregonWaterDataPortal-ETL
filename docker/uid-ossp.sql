e47640fa7dc35e257f5c7fee5283110a02444d59
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-12-16 11:41:37 -0500

    fail quick with pytest

--- docker/uid-ossp.sql
+++ docker/uid-ossp.sql
@@ -1,4 +1,6 @@
 -- init-uuid-ossp.sql
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- This is relevant but can't be ran until FROST is loaded and initializes the tables
 -- https://github.com/FraunhoferIOSB/FROST-Server/discussions/2047
 -- ALTER TABLE public."OBSERVATIONS" DISABLE TRIGGER datastreams_actualization_insert; 
