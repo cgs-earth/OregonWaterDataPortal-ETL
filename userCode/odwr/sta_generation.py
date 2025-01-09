49f445995ee8d94d83c4bdae42f4271ef919ccdc
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-01-09 11:35:20 -0500

    try generating id without the hash

--- userCode/odwr/sta_generation.py
+++ userCode/odwr/sta_generation.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-from userCode.common.lib import deterministic_hash
-
 
 from .types import Attributes, Datastream, Observation, StationData
 from userCode.common.ontology import ONTOLOGY_MAPPING
@@ -18,26 +16,25 @@ def to_sensorthings_observation(
     if datapoint is None:
         raise RuntimeError("Missing datapoint")
 
-    # generate a unique hash for the observation since we don't have any other way of
-    # uniquely identifying it from the upstream apii
-    # We have to use a hash since FROST limits the number of characters. We can't just
-    # concat the identifiers together sinec they might be too long
+    # generate a unique id by concatenating the datastream id and the resultTime
+    # we assume that the resultTime is in the format YYYY-MM-DDTHH:MM:SSZ and
+    # that when it is concat with the datastream id it will be unique and less than 18 characters in total
+    strippedResultTime = resultTime.removesuffix("Z")
+    assert strippedResultTime.endswith(
+        "00:00:00"
+    ), "resultTime does not end with 00:00:00 so we would lose information if we removed it"
+    strippedResultTime = strippedResultTime.removesuffix("00:00:00")
+    uniqueId = f"{associatedDatastream.iotid}{strippedResultTime}"
+    uniqueIdJustNumerical = "".join(filter(str.isdigit, uniqueId))
     MAX_LENGTH_IOTID_FOR_FROST = 18
-    id = f"{associatedDatastream.name}{datapoint}{resultTime}"
-    hashedId = deterministic_hash(id, MAX_LENGTH_IOTID_FOR_FROST)
-
-    ###### Other option that could theoretically work but can't have a guarantee of uniqueness if datapoint is a long float
-    ###### or a date with the seconds / minutes specified
-    # strippedResultTime = resultTime.removesuffix("Z")
-    # assert strippedResultTime.endswith("00:00:00"), "resultTime is not a UTC timestamp"
-    # strippedResultTime = strippedResultTime.removesuffix("00:00:00")
-    # uniqueId = f"{associatedDatastream.iotid}{datapoint}{strippedResultTime}"
-    # uniqueIdJustNumerical = "".join(filter(str.isdigit, uniqueId))
+    assert (
+        len(uniqueIdJustNumerical) <= MAX_LENGTH_IOTID_FOR_FROST
+    ), f"@iot.id {uniqueIdJustNumerical} is too long to insert into FROST when constructed with {associatedDatastream.iotid} and {strippedResultTime}"
 
     return Observation(
         **{
             "phenomenonTime": phenom_time,
-            "@iot.id": hashedId,
+            "@iot.id": uniqueIdJustNumerical,
             "resultTime": resultTime,
             "Datastream": {"@iot.id": associatedDatastream.iotid},
             "result": datapoint,
