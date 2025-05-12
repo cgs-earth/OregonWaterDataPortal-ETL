acd94b774375bda1dbfce619a573165d766dd35b
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-05-12 14:22:30 -0400

    Fix container monitor (#100)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docker/container_monitor/main.go
+++ docker/container_monitor/main.go
@@ -1,42 +1,34 @@
 package main
 
 import (
-	"log"
-	"os"
 	"strings"
 
+	"github.com/charmbracelet/log"
 	"github.com/docker/docker/client"
 )
 
 const tailLength = 20
 
-var logger *log.Logger
-
 func main() {
-	logger = log.New(os.Stdout, "", log.LstdFlags)
-
+	log.SetLevel(log.DebugLevel)
 	slackClient := NewSlackClient()
 	docker, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		logger.Fatalf("Error creating Docker client: %v\n", err)
+		log.Fatalf("Error creating Docker client: %v", err)
 	}
 	defer docker.Close()
 
-	logger.Print("Listening for Docker events...\n")
+	log.Info("Listening for Docker events...")
 
 	go func() {
 		watchAndHandleEvents(docker, *slackClient, tailLength)
 	}()
 
 	go func() {
-		patterns := strictEnv("LOG_WATCH_COMMA_SEPARATED_PATTERNS")
-
-		patternList := strings.Split(patterns, ",")
-
 		watchAndHandleLogs(docker, *slackClient, strictEnv("LOG_WATCH_CONTAINER_NAME"), LogWatcherConfig{
-			Patterns: patternList,
+			Patterns: strings.Split(strictEnv("LOG_WATCH_COMMA_SEPARATED_PATTERNS"), ","),
 		})
 	}()
-	// Block forever 
+	// Block forever
 	select {}
 }
