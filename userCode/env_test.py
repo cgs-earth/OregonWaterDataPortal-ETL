c0233c347c6a866dd697edad45488fc511e45379
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-06-30 14:30:38 +0000

    fixSlackInProd

--- userCode/env_test.py
+++ userCode/env_test.py
@@ -1,6 +1,7 @@
 from dagster import asset
 from dagster_slack import SlackResource
 from userCode.env import RUNNING_AS_TEST_OR_DEV
+from userCode.util import get_env
 
 
 def test_running_as_dev():
@@ -16,6 +17,7 @@ def slack_message_test(slack: SlackResource):
     is harder to test in as a unit test. As such it should be ran
     and tested in the dagster UI
     """
+    assert get_env("SLACK_BOT_TOKEN")
     slack.get_client().chat_postMessage(
         channel="#cgs-iow-bots", text=":wave: hey there!"
     )
